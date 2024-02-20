6fbe8f21d72ebfa810923462ee1e3a2da315dd5d
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-02-20 11:30:04 +0100

    Hero and custom elements

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,10 +1,4 @@
 import curryDecorator from '../../libs/curry-decorate/curry-decorate.js';
+import { Hero } from '../../libs/hero/hero.js';
 
-export class Hero extends HTMLElement {
-  connectedCallback() {
-    console.log('Hero connected');
-    if (this.onComponentComplete) this.onComponentComplete(this);
-  }
-}
-
-export default curryDecorator('raqn-hero', Hero);
+export default curryDecorator('raqn-hero', class extends Hero {});
