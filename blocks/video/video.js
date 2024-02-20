7c05d685a3a8c7bb54bfddcc5ba3ef844d3cd6db
Joan Beltran <joan.beltran@partners.basf.com>
2024-02-20 14:44:39 +0100

    use imported blocks

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,1 +1,7 @@
+import originalDecorate from 'libs/blocks/eddys-video/eddys-video.js';
+
 console.log('video component');
+
+export default async function decorate(block) {
+    await originalDecorate(block);
+}
