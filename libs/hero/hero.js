6b25415be680f0f8aaf7a6df2e3a3301fd210f59
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-02-20 11:32:56 +0100

    Hero and custom elements

--- libs/hero/hero.js
+++ libs/hero/hero.js
@@ -1,9 +1,16 @@
 import curryDecorator from '../../libs/curry-decorate/curry-decorate.js';
 
 export class Hero extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
   connectedCallback() {
-    console.log('Hero connected');
-    if (this.onComponentComplete) this.onComponentComplete(this);
+    this.shadowRoot.innerHTML = `<link rel="stylesheet" href="${import.meta.url.replace(
+      '.js',
+      '.css',
+    )}">`;
+    this.shadowRoot.append(...this.children);
   }
 }
 
