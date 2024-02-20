9d3b95910c5d1ca4a18ae260607aefc984c68726
Felipe Simoes <felipe.simoes@netcentric.biz>
2024-02-20 11:21:45 +0100

    Hero and custom elements

--- libs/curry-decorate/curry-decorate.js
+++ libs/curry-decorate/curry-decorate.js
@@ -1,17 +1,26 @@
 function setupElement(elementName, block) {
   const element = document.createElement(elementName);
+  element.uuid = `gen${crypto.randomUUID().split('-')[0]}`;
+  element.onComponentComplete = block.onComponentComplete;
   element.append(...block.children);
   block.replaceWith(element);
+  return block;
 }
 
-export default function curryDecorator(elementName, contructor) {
-  return function decorate(block) {
-    if (customElements.get(elementName)) {
-      setupElement(elementName, block);
-    } else {
-      customElements.define(elementName, contructor);
-      window.raqnComponents[elementName] = contructor;
-      setupElement(elementName, block);
-    }
+export default function curryDecorator(elementName, contructor, async = false) {
+  return async function decorate(block) {
+    return new Promise((resolve) => {
+      block.onComponentComplete = (el) => {
+        resolve(el);
+      };
+      if (customElements.get(elementName)) {
+        const el = setupElement(elementName, block);
+      } else {
+        customElements.define(elementName, contructor);
+        if (!window.raqnComponents) window.raqnComponents = {};
+        window.raqnComponents[elementName] = contructor;
+        const el = setupElement(elementName, block);
+      }
+    });
   };
 }
